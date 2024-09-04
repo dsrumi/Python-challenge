7a114c36e6fe7c5a2d85b0d3aa8cb053d84205cc
Iram Anwar <iram.raza23@gmail.com>
2024-09-03 23:43:46 -0400

    Updated pypoll files

--- pypoll/main.py
+++ pypoll/main.py
@@ -2,6 +2,17 @@ import os
 import csv
 poll_csv = os.path.join('.','Resources','election_data.csv')
 with open(poll_csv, 'r') as csvfile:
+    folder_name = "Analyses"
+
+    # Create the folder if it doesn't exist
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    # Define the file path within the folder
+    file_path = os.path.join(folder_name, "output.txt")
+    f = open(file_path, 'w')
+    
+    
     # defining variables
     csvreader = csv.reader(csvfile)
     total = -1
@@ -24,21 +35,26 @@ with open(poll_csv, 'r') as csvfile:
     diana_percentage =round(diana_votes/total * 100 ,3)
     raymon_percentage = round(raymon_votes/total * 100 ,3)
 
-    # print percentages and votes
-    print("Election Results")
-    print("------------------------")
-    print(f"Total votes: {total}")
-    print("------------------------")
-    print(f"Charles Casper Stockham: {charles_percentage}% ({charles_votes})")
-    print(f"Diana DeGette: {diana_percentage}% ({diana_votes})")
-    print(f"Raymon Anthony Doane: {raymon_percentage}% ({raymon_votes})")
+    # write percentages and votes to file
+    f.writelines("Election Results\n")
+    f.writelines("------------------------\n")
+    f.writelines(f"Total votes: {total}\n")
+    f.writelines("------------------------\n")
+    f.writelines(f"Charles Casper Stockham: {charles_percentage}% ({charles_votes})\n")
+    f.writelines(f"Diana DeGette: {diana_percentage}% ({diana_votes})\n")
+    f.writelines(f"Raymon Anthony Doane: {raymon_percentage}% ({raymon_votes})\n")
 
-    # finding and printing the winner
+    # finding and writing the winner to our file
     winner_votes = max(charles_votes,diana_votes,raymon_votes)
     if winner_votes == charles_votes:
-        print("Winner: Charles Casper Stockham ")
+        f.writelines("Winner: Charles Casper Stockham\n ")
     elif winner_votes == diana_votes:
-        print("Winner: Diana DeGette")
+        f.writelines("Winner: Diana DeGette\n")
     else:
-        print("Winner: Raymon Anthony Doane")
-    print("------------------------")
+        f.writelines("Winner: Raymon Anthony Doane\n")
+    f.writelines("------------------------\n")
+
+
+f.close()
+
+
